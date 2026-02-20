d41afd8cdd555dcc793d0865e9dfa8c3a5779f50
Jose2511-gift <jv.delei@gmail.com>
2026-02-19 22:49:40 -0300

    Update garcom.py

--- garcom.py
+++ garcom.py
@@ -63,6 +63,7 @@ def analisar(estrategia: str = "z", id: str = "0"):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
+
 
 
